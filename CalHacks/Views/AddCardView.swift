3cb41ad908dfe92f6d39d15704313678c0e2b13d
Andrew Zheng <aheze@getfind.app>
2023-10-28 21:53:39 -0700

    Work on add cards

--- CalHacks/Views/AddCardView.swift
+++ CalHacks/Views/AddCardView.swift
@@ -10,33 +10,34 @@ import SwiftUI
 struct AddCardView: View {
     @ObservedObject var model: ViewModel
     @Environment(\.dismiss) var dismiss
+    @State var shown = false
 
     var body: some View {
-        VStack {
-            HStack {
-                Text("Add Card")
-                    .font(.largeTitle)
-                    .textCase(.uppercase)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+        VStack(spacing: 24) {
+            VStack {
+                HStack {
+                    Text("Add Card")
+                        .font(.largeTitle)
+                        .textCase(.uppercase)
+                        .frame(maxWidth: .infinity, alignment: .leading)
 
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "xmark")
-                        .foregroundColor(.white)
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .frame(width: 42, height: 42)
-                        .background(
-                            Circle()
-                                .fill(Color.white)
-                                .opacity(0.15)
-                        )
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark")
+                            .foregroundColor(.white)
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .frame(width: 42, height: 42)
+                            .background(
+                                Circle()
+                                    .fill(Color.white)
+                                    .opacity(0.15)
+                            )
+                    }
                 }
-            }
 
-            Button {} label: {
-                VStack(spacing: 12) {
+                Button {} label: {
                     VStack(spacing: 24) {
                         Image(systemName: "camera.fill")
 
@@ -53,7 +54,7 @@ struct AddCardView: View {
                                 LinearGradient(
                                     colors: [
                                         UIColor.systemTeal.color,
-                                        UIColor.systemTeal.offset(by: -0.2).color
+                                        UIColor.systemTeal.offset(by: 0.1).color
                                     ],
                                     startPoint: .topLeading,
                                     endPoint: .bottomTrailing
@@ -61,21 +62,45 @@ struct AddCardView: View {
                             )
                             .brightness(-0.1)
                     }
-                    
-                    Text("Scan a 3D object from the real world!")
-                        .font(.body)
                 }
             }
+            .padding(.horizontal, 20)
+            .padding(.top, 24)
 
-            Spacer()
+            ScrollView {
+                LazyVStack(alignment: .leading, spacing: 20) {
+                    Text("Card Store")
+                        .font(.largeTitle)
+                        .textCase(.uppercase)
+
+                    ForEach(Array(zip(model.cardsStore.indices, model.cardsStore)), id: \.1.id) { index, card in
+                        CardView(card: card)
+                            .scaleEffect(shown ? 1 : 0.9)
+                            .opacity(shown ? 1 : 0)
+                            .animation(.spring(response: 0.2, dampingFraction: 1, blendDuration: 1).delay(Double(index) * 0.1), value: shown)
+                    }
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, 24)
+                .padding(.bottom, 32)
+            }
+            .background {
+                RoundedRectangle(cornerRadius: 32)
+                    .fill(Color.white)
+                    .opacity(0.1)
+                    .padding(.bottom, -100)
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .padding(.horizontal, 20)
-        .padding(.vertical, 24)
         .background {
             Color.black
                 .brightness(0.1)
                 .ignoresSafeArea()
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
+                shown = true
+            }
         }
     }
 }
